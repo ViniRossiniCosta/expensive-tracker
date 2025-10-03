89411016f59c6d5aab7194020861abea9a842128
ViniRossiniCosta <rossinicostavinicius@gmail.com>
2025-10-03 08:43:36 -0300

    first commit

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,43 @@
+<template>
+    <h3>Add new Transaction</h3>
+    <form id="form" @submit.prevent="onSubmit">
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" id="text" placeholder="Enter Text..." v-model="Text" />
+        </div>
+        <div class="form-class">
+            <label for="amount">Amount (Negative - Income, Positive - Income)</label>
+            <input type="text" id="amount" placeholder="Enter amount..." v-model="Amount" />
+        </div>
+        <button class="btn">Add Transaction</button>
+    </form>
+</template>
+<script setup>
+import { useToast } from 'vue-toastification';
+import { ref } from 'vue';
+
+const text = ref('');
+const amount = ref('');
+
+const toast = useToast();
+
+const emit = defineEmits('transactionSubmitted');
+
+const onSubmit = () => {
+    if(!text.value || !amount.value) {
+        toast.error('Both field must be filled');
+        return;
+    }
+}
+
+const transactionData = {
+    text: text.value,
+    amount: parseFloat(amount.value),
+};
+
+emit('transactionSubmitted', transactionData);
+
+text.value = '';
+amount.value = '';
+
+</script>
